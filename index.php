a082ebcf4b52c22b5cd4ed98b994505106247b87
kirandash <thekirankumardash@gmail.com>
2016-07-22 20:36:24 +0530

    2-The-add_shortcode-Method

--- index.php
+++ index.php
@@ -7,4 +7,9 @@ Version: 1.0
 Author: Kiran Dash
 Author URI: http://bgwebagency.com/
 */
+
+add_shortcode('twitter', function(){
+	//echo should not be used in add shortcode. Please use return
+	return '<a href="">Follow me on twitter</a>';
+});
 ?>
