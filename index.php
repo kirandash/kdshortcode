e1104a31a7c85535e3c4b89a95c4017df44b3593
kirandash <thekirankumardash@gmail.com>
2016-07-22 21:04:00 +0530

    5-The-Shortcode_atts-Function

--- index.php
+++ index.php
@@ -10,8 +10,16 @@ Author URI: http://bgwebagency.com/
 
 add_shortcode('twitter', function($atts, $content){
 	//echo should not be used in add shortcode. Please use return
-	if(!isset($atts['username'])) $atts['username'] = 'thekirandash';
-	if(empty($content)) $content = 'Follow me on twitter';
-	return '<a href="http://twitter.com/'.$atts['username'].'">'.$content.'</a>';
+	//if(!isset($atts['username'])) $atts['username'] = 'thekirandash';
+	//if(empty($content)) $content = 'Follow me on twitter';
+	$atts = shortcode_atts(
+		array(
+			'username' => 'thekirandash',
+			'content' => !empty($content)?$content:'Follow me on twitter'
+		), $atts
+	);
+	
+	extract($atts); // PHP function to extract attributes to variables
+	return "<a href='http://twitter.com/$username'>$content</a>";
 });
 ?>
