c582d1bd93d27eb1100d71371e3b15b7a6054daa
kirandash <thekirankumardash@gmail.com>
2016-07-22 20:31:38 +0530

    1-Making-the-Plugin-Visible-to-WordPress

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Twitter Shortcode
+Plugin URI: http://bgwebagency.com/
+Description: Simple shortcode
+Version: 1.0
+Author: Kiran Dash
+Author URI: http://bgwebagency.com/
+*/
+?>
